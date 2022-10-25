1afe639643b44267b28f37cc8d2f0a2342d3efe0
Strobo90 <113635128+Strobo90@users.noreply.github.com>
2022-10-24 20:40:01 -0600

    90

--- parachute.cs
+++ parachute.cs
@@ -7,33 +7,26 @@ namespace Jumper_Idaho
         private int count;
         private int trueTries = 0; // tries are intialize to be zero and set as private so that they cant be change # encupsulated
 
-
-        public parachute()
-        {
-            
-            
-            Parachute.Add(" ___");
+        public parachute() //print the below parachute to the terminal 
+        {   Parachute.Add(" ___");
             Parachute.Add("/___\\");
             Parachute.Add("\\   /");
             Parachute.Add(" \\ /");
             Parachute.Add("  O");
             Parachute.Add(" /|\\");
             Parachute.Add(" / \\");
-
             Parachute.Add("\n !!! Guess wrong You die | \n Guess Smart you live !!!");
-        }
-
+        }  // check the inputs of the player 
         public bool checkInput(List<char> guesses, string currentguess){
             if (guesses.Contains(currentguess[0])){
-                Console.WriteLine(" OOOPS !! Try another letter!");
+                Console.WriteLine(" OOOPS !! Try another letter!");// if the same guess is made twice print this
                 return true;
-            }
+            } // enter this blockif the above returns a false boolean
             else {
                 return false;
             }
 
-        }
-
+        }               // Lets check the number of tries and the Guesses
         public bool checkJumper(List<char> wordGuess, int tries){
             count = 0;
             for(int i=0;i<wordGuess.Count;i++){
@@ -46,11 +39,11 @@ namespace Jumper_Idaho
                 return false;
                 
             }
-            else if(tries == 4){
+            else if(tries == 4){ // once the tries equates to 4 return a false value to check jumper and end game
                 return false;
             }
             else {
-                return true;
+                return true; // if less than 4 tries made . continue trying
             }
         }
 
@@ -60,7 +53,7 @@ namespace Jumper_Idaho
             }
             else if(tries == 4){
                 Parachute.RemoveRange(0,1);
-                Parachute[0] = "   X";
+                Parachute[0] = "DEAD MAN GAME OVER ";
 
 
             }
